35b1bca1106926e5e0ad61579ccd66a453f743a5
B.J. Johnson <cmsi.bj.lmu@gmail.com>
2023-08-31 14:14:54 -0700

    adding source file example

---
+++ test5/test6/sayhello.c
@@ -0,0 +1,14 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * filename: sayhello.c
+ * purpose:  just to get things rolling
+ * @author:  Dr. Johnson
+ * @date:    2020-12-31
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+ #include <stdio.h>
+
+ int main( int c, char * argv[] ) {
+
+    printf( "Hello, world!" );
+
+ }
