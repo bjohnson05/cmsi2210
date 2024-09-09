7cc7ed1ccc90541aab2146ab1ca693681f07c767
B.J. Johnson <bjohnson05@gmail.com>
2024-09-08 23:00:10 -0700

    Update Repository for Fall 2024 again

--- classwork/classwork01/sayhello.c
+++ classwork/classwork01/sayhello.c
@@ -4,11 +4,10 @@
  * @author:  Dr. Johnson
  * @date:    2020-12-31
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
- #include <stdio.h>
+   #include <stdio.h> 
 
  int main( int c, char * argv[] ) {
 
-    printf( "Hello, world!" );
+    printf( "\n\n   Hello, world!\n\n" );
 
  }
